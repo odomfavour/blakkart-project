6649eab159caf550b15a48296fcad2282b66e9ac
odomfavour <ogochukwuodom@gmail.com>
2022-12-16 20:47:45 +0100

    ch: latest changes after npm run generate

---
+++ dist/_nuxt/da2a74b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[58],{590:function(n,l,e){"use strict";e.r(l);var t={layout:"seller"},o=e(27),component=Object(o.a)(t,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);l.default=component.exports}}]);
